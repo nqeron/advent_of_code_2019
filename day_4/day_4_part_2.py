34662092512920eacefe25f42303de6605b4ce76
nqeron <nqeron@gmail.com>
2019-12-10 23:24:38 -0500

    Day 4 - finished

--- day_4/day_4_part_2.py
+++ day_4/day_4_part_2.py
@@ -14,6 +14,9 @@ def check_for_double(stringed) -> bool:
             if len(stringed) > s+2 and stringed[s+2] == stringed[s]:
                 s += 2
                 continue
+            elif s > 1 and stringed[s-1] == stringed[s]:
+                s += 1
+                continue
             else:
                 return True
         s += 1
@@ -21,7 +24,6 @@ def check_for_double(stringed) -> bool:
 
 
 def analyze():
-    print(check_for_double(str(333222)))
     print(sum([1 for i in range(197487, 673252) if check_for_double(str(i)) and check_for_descending(str(i))]))
 
 
