f9b0222b59958ab31386b18454ac4167454e5530
xbbnw7r <Noah.Fields@bnymellon.com>
2019-12-18 17:23:58 -0500

    Day 14 - part 2 - incomplete

--- day_14/day_14_part_2.py
+++ day_14/day_14_part_2.py
@@ -1,10 +1,20 @@
 import re
 from collections import defaultdict, namedtuple
+import typing
+import math
 
 Reaction = namedtuple("Reaction", "quantity name")
 
 
-def analyze(file):
+def can_be_made(reactions: typing.List[Reaction], current_amount) -> int:
+    react_dict = defaultdict(lambda: 0)
+    react_dict.update({r.name: r.quantity for r in reactions})
+    # print([current_amount[elem] // react_dict[elem] for elem in current_amount if react_dict[elem] > 0])
+    return min([current_amount[elem] // react_dict[elem] if react_dict[elem] > 0 else 0 for elem in current_amount
+                if react_dict[elem] > 0])
+
+
+def analyze(file, given_ore):
     line_matcher = re.compile(r"(\d+) (\w+)")
     chemistry = {}
     with open(file) as f:
@@ -17,7 +27,8 @@ def analyze(file):
     amount_stack = [i.quantity * 1 for i in chemistry["FUEL"][:-1]]
     total_ore = 0
     current_amount = defaultdict(lambda: 0)
-    prev_item = Reaction(1, "FUEL")
+    distance_to_fuel = defaultdict(lambda: 0)
+    # calc_distance = True
     while len(to_produce) > 0:
         next_production = to_produce.pop()
         next_amount = amount_stack.pop()
@@ -36,12 +47,22 @@ def analyze(file):
             for i in add_to_produce:
                 to_craft_i = i.quantity * crafted  # - current_amount[i.name]
                 # current_amount[i.name] += to_craft_i
+                # if calc_distance:
+                distance_to_fuel[i.name] = distance_to_fuel[next_production.name] + 1
                 amount_stack.append(to_craft_i)
 
             current_amount[next_production.name] += crafted * produced - needed - current_amount[next_production.name]
-            prev_item = next_production
-    print(current_amount["ORE"])
+
+    print(distance_to_fuel)
+    first_crafts = given_ore // current_amount["ORE"]
+    current_amount.update({i: current_amount[i] * first_crafts for i in current_amount})
+    current_amount["FUEL"] += first_crafts
+    current_amount["ORE"] = given_ore - current_amount["ORE"]
+    # print(current_amount)
+
+    next_product = min([(product, can_be_made(chemistry[product][:-1], current_amount)) for product in chemistry],
+                       key=lambda x: distance_to_fuel[x[0]] if x[1] > 0 else math.inf)
 
 
 if __name__ == '__main__':
-    analyze("inputs/day_14_test.txt")
+    analyze("inputs/day_14_test2.txt", 1000)
